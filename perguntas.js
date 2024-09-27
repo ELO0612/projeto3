65513837ba1d807b68ad5f6a206c3da74ab1901b
ELO0612 <148974926+ELO0612@users.noreply.github.com>
2024-09-27 20:24:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+const perguntas = [
+    {
+        pergunta: 'Qual é a raça de gato mais popular do mundo?',
+        resposta: 'O gato doméstico é a raça mais popular.'
+    },
+    {
+        pergunta: 'Por que os gatos ronronam?',
+        resposta: 'Os gatos ronronam para mostrar contentamento ou para se acalmar.'
+    },
+    {
+        pergunta: 'Quantas horas por dia um gato normalmente dorme?',
+        resposta: 'Os gatos dormem cerca de 12 a 16 horas por dia.'
+    },
+    {
+        pergunta: 'Qual é a expectativa de vida média de um gato?',
+        resposta: 'A expectativa de vida média de um gato é de 12 a 15 anos.'
+    },
+    {
+        pergunta: 'Por que os gatos gostam de arranhar?',
+        resposta: 'Os gatos arranham para marcar território e manter as garras afiadas.'
+    },
+    {
+        pergunta: 'Como se chama o órgão responsável pelo olfato aguçado dos gatos?',
+        resposta: 'O órgão de Jacobson é responsável pelo olfato aguçado dos gatos.'
+    },
+    {
+        pergunta: 'Os gatos são animais solitários?',
+        resposta: 'Embora sejam independentes, os gatos também podem ser sociais e formarem laços fortes com humanos.'
+    },
+    {
+        pergunta: 'Qual é a diferença entre um gato siames e um gato persa?',
+        resposta: 'Gatos siameses têm pelagem curta e são conhecidos por suas marcas, enquanto persas têm pelagem longa e densa.'
+    }
+];
+
